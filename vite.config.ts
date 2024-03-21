8c960acefd1aac15585ae1fddb000534bc0e6065
Mathius <info@yogimathius.dev>
2024-03-20 22:09:01 -0700

    feat: add builder io

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { builderDevTools } from "@builder.io/dev-tools/vite";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [remix(), tsconfigPaths(), builderDevTools()],
 });
